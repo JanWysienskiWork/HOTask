b14abdb9f4737a5160e073ba07235f3e30962f35
Jan <99261051+JanWysienskiWork@users.noreply.github.com>
2023-02-23 10:49:51 +0100

    minor fix

--- HexOceanTask/settings.py
+++ HexOceanTask/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1b*4nqfau-up%ots%6&e55id9d68fm=af(i5wu17na0pyts)ox'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
